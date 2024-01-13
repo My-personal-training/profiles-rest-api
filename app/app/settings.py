7743901c07122cc85860261f9eb1ed44ccd6cff0
Lorenzo Arias Villegas <santyago.arias.sav@gmail.com>
2024-01-12 23:39:05 -0400

    Added custom user profile model manager and migrations

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# User model customisation
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
